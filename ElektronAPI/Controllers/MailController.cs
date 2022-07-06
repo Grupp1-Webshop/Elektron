4dd0e7ed90d4e3e1d5cd427da41c3fa43a803677
djzzz <djzzz.fixed@gmail.com>
2022-07-06 23:47:36 +0200

    Mail system

--- ElektronAPI/Controllers/MailController.cs
+++ ElektronAPI/Controllers/MailController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ElektronAPI.Models.Mail;
+using MailKit.Security;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace ElektronAPI.Controllers
 {
+    [EnableCors("Api")]
+    [Route("api/[controller]")]
+    [ApiController]
     public class MailController : Controller
     {
-        public IActionResult Index()
+        // POST: api/mail
+        [HttpPost]
+        public async Task<ActionResult> Index(CreateMailViewModel createMailViewModel)
         {
-            return View();
+            if (createMailViewModel == null)
+            {
+                return BadRequest();
+            }
+            MailMessage mail = new MailMessage("Elektron.Lexicon@outlook.com", "Elektron.Lexicon@outlook.com", createMailViewModel.Subject, createMailViewModel.Subject);
+            using var smtp = new SmtpClient("smtp-mail.outlook.com");
+            smtp.Port = 587;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.EnableSsl = true;
+            System.Net.NetworkCredential credentials =
+            new System.Net.NetworkCredential("Elektron.Lexicon@outlook.com", "Uk?-fK74.mCT/(7k");
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = credentials;
+            smtp.Send(mail);
+            return Ok();
         }
     }
 }
---
+++ ElektronAPI/Models/Mail/CreateMailViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ElektronAPI.Models.Mail
+{
+    public class CreateMailViewModel
+    {
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+    }
+}

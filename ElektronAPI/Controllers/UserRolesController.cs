45208d10399043d955adf326f502d03cf964624a
djzzz <djzzz.fixed@gmail.com>
2022-07-08 10:09:23 +0200

    Added promotion system

---
+++ ElektronAPI/Controllers/UserRolesController.cs
@@ -0,0 +1,48 @@
+﻿using ElektronAPI.Models.Identity;
+using ElektronAPI.Models.UserRoles;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ElektronAPI.Controllers
+{
+    [EnableCors("Api")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserRolesController : Controller
+    {
+        private RoleManager<IdentityRole> _roleManager;
+        private UserManager<ApplicationUser> _userManager;
+
+        public UserRolesController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<ApplicationUser>>> Index(UserRolesViewModel userRolesViewModel)
+        {
+            var admin = await _roleManager.FindByIdAsync(userRolesViewModel.RoleId);
+            var user = await _userManager.FindByIdAsync(userRolesViewModel.UserId);
+            if (admin == null)
+            {
+                return NotFound();
+            }
+            if (user == null)
+            {
+                return NotFound();
+            }
+            IdentityResult result = await _userManager.AddToRoleAsync(user, admin.Name);
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ ElektronAPI/Models/UserRoles/UserRolesViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ElektronAPI.Models.UserRoles
+{
+    public class UserRolesViewModel
+    {
+        public string RoleId { get; set; }
+        public string UserId { get; set; }
+    }
+}

5fd4acc1b30752d3f412aa1e754e8fcc0e471685
Marek <98730338+Marek-Piotrowski@users.noreply.github.com>
2022-07-12 16:59:04 +0200

    Email notification added after new contact query.

--- client/src/pages/ContactForm.js
+++ client/src/pages/ContactForm.js
@@ -1,4 +1,5 @@
 import { Grid, TextField, Button, Box } from "@mui/material";
+import axios from "axios";
 import React, { useState } from "react";
 
 const defaultValues = {
@@ -13,7 +14,34 @@ export const ContactForm = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    const url = "http://localhost:5207/api/Mail";
     console.log(formValues);
+
+    let emailTemplate = `
+
+        New contact query from  ${formValues.name}
+
+        Client email:  ${formValues.email}
+
+        Client message:
+
+        ${formValues.message}
+
+`;
+
+    let data = {
+      subject: formValues.subject,
+      body: emailTemplate,
+    };
+
+    await axios
+      .post(url, data, {
+        headers: {
+          "content-type": "application/json",
+        },
+      })
+      .then((res) => console.log(res))
+      .catch((error) => console.log(error));
   };
 
   const handleInputChange = (e) => {
@@ -33,53 +61,63 @@ export const ContactForm = () => {
         direction="column"
         sx={{ border: 1, borderRadius: 2 }}
       >
-        <Grid item sx={{ mb: 2 }}>
+        <Grid item>
           <Box
             alignItems="center"
             justifyContent="center"
-            sx={{ mt: 2, mb: 2, fontSize: 20 }}
+            sx={{ mb: 3, mt: 3, fontSize: 24 }}
           >
             <div>Kontakta oss</div>
           </Box>
         </Grid>
-        <Grid item sx={{ mb: 2 }}>
+        <Grid item>
           <TextField
             id="name-input"
             name="name"
             label="Name"
             type="text"
+            variant="outlined"
             value={formValues.name}
             onChange={handleInputChange}
+            sx={{ mb: 2, width: 500, maxWidth: "100%" }}
           />
         </Grid>
-        <Grid item sx={{ mb: 2 }}>
+        <Grid item>
           <TextField
             id="subject-input"
             name="subject"
             label="Subject"
             type="text"
             value={formValues.subject}
+            variant="outlined"
             onChange={handleInputChange}
+            sx={{ mb: 2, width: 500, maxWidth: "100%" }}
           />
         </Grid>
-        <Grid item sx={{ mb: 2 }}>
+        <Grid item>
           <TextField
             id="email-input"
             name="email"
             label="Email"
             type="email"
+            variant="outlined"
             value={formValues.email}
             onChange={handleInputChange}
+            sx={{ mb: 2, width: 500, maxWidth: "100%" }}
           />
         </Grid>
-        <Grid item sx={{ mb: 2 }}>
+        <Grid item>
           <TextField
             id="message-input"
             name="message"
             label="Message"
             type="text"
+            multiline
+            minRows={4}
+            variant="outlined"
             value={formValues.message}
             onChange={handleInputChange}
+            sx={{ mb: 2, width: 500, maxWidth: "100%" }}
           />
         </Grid>
 
@@ -89,7 +127,7 @@ export const ContactForm = () => {
           type="submit"
           sx={{ mb: 3, minWidth: 200, height: 45 }}
         >
-          Send
+          Skicka
         </Button>
       </Grid>
     </form>

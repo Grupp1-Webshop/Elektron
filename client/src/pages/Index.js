833f3f105728be07f9ed5ff2c6d1971d4ff778e2
kvastfeningen <98735892+kvastfeningen@users.noreply.github.com>
2022-07-13 12:40:13 +0200

    rmssk

--- client/src/pages/Index.js
+++ client/src/pages/Index.js
@@ -1,4 +1,32 @@
-export function Index(){
-    return <p>index</p>
+import React from "react";
+import { useEffect } from "react";
+
+import { Main } from "../compontents"
+//import { ProductList } from "../containers/ProductList";
+//import { UseProduct } from "../Hooks/UseProduct";
+import { CategoryList } from "../containers/CategoryList";
+import { UseCategories } from "../Hooks/UseCategories";
+
+
+export function Index() {
+    		
+    const { getCategories, categories } = UseCategories();
+    useEffect(() => {
+        getCategories()
+        
+    }, []);
+
+    return <Main>
+
+        <Main.Title>{"Categories"}</Main.Title>
+        <Main.Content>
+        {
+                categories !== null ? (
+                    <CategoryList categories={categories} max={0} />
+                ) : ('')
+            }
+
+        </Main.Content>
+    </Main>
     
 }

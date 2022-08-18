9891dca5580148404c6d05a1b457e13340f10953
Marek <98730338+Marek-Piotrowski@users.noreply.github.com>
2022-08-18 21:52:24 +0200

    small fix

--- client/src/containers/Footer.js
+++ client/src/containers/Footer.js
@@ -1,17 +1,21 @@
-import { Menu, Logotype } from "../compontents"
-import { Copyright } from "../compontents/Copyright/style/Copyright"
-export function Footer(){
-    return <footer>
-        <Logotype>
-            <Logotype.Img></Logotype.Img>
-        </Logotype>
-        <Copyright white>Electron © Copyright 2022</Copyright>
-        <div className="menu">
-            <Menu>
-                <Menu.List>
-                    <Menu.Item white to="/">Statsida</Menu.Item>
-                </Menu.List>
-            </Menu>
-        </div>
+import { Menu, Logotype } from "../compontents";
+import { Copyright } from "../compontents/Copyright/style/Copyright";
+export function Footer() {
+  return (
+    <footer>
+      <Logotype>
+        <Logotype.Img></Logotype.Img>
+      </Logotype>
+      <Copyright white>Electron © Copyright 2022</Copyright>
+      <div className="menu">
+        <Menu>
+          <Menu.List>
+            <Menu.Item white to="/">
+              Startsida
+            </Menu.Item>
+          </Menu.List>
+        </Menu>
+      </div>
     </footer>
+  );
 }
--- client/src/pages/Index.js
+++ client/src/pages/Index.js
@@ -7,27 +7,25 @@ import { UseProduct } from "../Hooks/UseProduct";
 import { UseCategories } from "../Hooks/UseCategories";
 import { Main } from "../compontents";
 export function Index() {
-  const {getCategories, categories} = UseCategories();
-  const {getProducts, product} = UseProduct();
+  const { getCategories, categories } = UseCategories();
+  const { getProducts, product } = UseProduct();
   useEffect(() => {
-    getCategories()
-    getProducts()
-  },[])
+    getCategories();
+    getProducts();
+  }, []);
   return (
     <>
       <Main>
         <Main.Title>Produkter</Main.Title>
         <Main.Content>
-          <ProductList products={product}/>
+          <ProductList products={product} />
         </Main.Content>
         <Main.Title>Kategorier</Main.Title>
         <Main.ThreeCol>
-          <CategoryList categories={categories}/>
+          <CategoryList categories={categories} />
         </Main.ThreeCol>
       </Main>
       <Contact />
-      
-      
     </>
   );
 }
